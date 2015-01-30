d6a5bb73aeead974f0ada20f64578b423ccdf600
Zachary Palmer <zep_git@bahj.com>
2015-01-29 19:48:19 -0500

    More work on standalone Smelt.  No viable docking framework seems to compare to Eclipse.

--- Smelt/src/main/java/com/bahj/smelt/plugin/builtin/basegui/SmeltTabPanel.java
+++ Smelt/src/main/java/com/bahj/smelt/plugin/builtin/basegui/SmeltTabPanel.java
@@ -1,14 +1,42 @@
 package com.bahj.smelt.plugin.builtin.basegui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import com.bahj.smelt.plugin.builtin.basegui.context.GUITabKey;
 
 public class SmeltTabPanel extends JPanel {
     private static final long serialVersionUID = 1L;
+    
+    private Set<GUITabKey> keysUsed;
+    private JTabbedPane tabbedPane;
 
     public SmeltTabPanel() {
         this.setMinimumSize(new Dimension(400,300));
-        // TODO
+        
+        this.keysUsed = new HashSet<>();
+        
+        this.tabbedPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+        this.setLayout(new BorderLayout());
+        this.add(this.tabbedPane, BorderLayout.CENTER);
+        
+        
+    }
+    
+    public void ensureTab(GUITabKey key, JComponent contents) {
+        if (this.keysUsed.contains(key)) {
+            return;
+        }
+        
+        // Create a new tab containing the provided contents.
+        this.tabbedPane.insertTab(key.getTitle(), null, contents, null, this.tabbedPane.getTabCount());
+        
+        // Change the header for the tab so that it has a closing 
     }
 }
--- Smelt/src/main/java/com/bahj/smelt/plugin/builtin/basegui/context/GUIExecutionContext.java
+++ Smelt/src/main/java/com/bahj/smelt/plugin/builtin/basegui/context/GUIExecutionContext.java
@@ -24,6 +24,7 @@ public class GUIExecutionContext {
      * @param component The body of the tab.
      */
     public void ensureTab(GUITabKey key, JComponent contents) {
+        
         // TODO
     }
 }
