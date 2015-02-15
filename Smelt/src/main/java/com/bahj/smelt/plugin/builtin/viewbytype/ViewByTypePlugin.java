e100109788d1fc42cf66d4fc9e13222ee576d928
Zachary Palmer <zep_git@bahj.com>
2015-02-15 16:17:09 -0500

    Added card layout to hide object view tree when database is not loaded.

--- Smelt/src/main/java/com/bahj/smelt/plugin/builtin/viewbytype/ViewByTypePlugin.java
+++ Smelt/src/main/java/com/bahj/smelt/plugin/builtin/viewbytype/ViewByTypePlugin.java
@@ -1,11 +1,21 @@
 package com.bahj.smelt.plugin.builtin.viewbytype;
 
+import java.awt.CardLayout;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import bibliothek.gui.dock.common.DefaultMultipleCDockable;
+import bibliothek.gui.dock.common.MultipleCDockable;
+import bibliothek.gui.dock.common.event.CDockableStateListener;
+import bibliothek.gui.dock.common.intern.CDockable;
+import bibliothek.gui.dock.common.mode.ExtendedMode;
 
 import com.bahj.smelt.SmeltApplicationModel;
 import com.bahj.smelt.event.SmeltApplicationConfigurationLoadedEvent;
@@ -20,6 +30,9 @@ import com.bahj.smelt.plugin.builtin.basegui.execution.BasicGUIPosition;
 import com.bahj.smelt.plugin.builtin.basegui.execution.DockableIdentifier;
 import com.bahj.smelt.plugin.builtin.basegui.execution.GUIExecutionContext;
 import com.bahj.smelt.plugin.builtin.data.model.DataModelPlugin;
+import com.bahj.smelt.plugin.builtin.data.model.event.DataModelPluginEvent;
+import com.bahj.smelt.plugin.builtin.data.model.event.DatabaseClosedEvent;
+import com.bahj.smelt.plugin.builtin.data.model.event.DatabaseOpenedEvent;
 import com.bahj.smelt.plugin.builtin.editor.EditorModel;
 import com.bahj.smelt.plugin.builtin.editor.EditorPlugin;
 import com.bahj.smelt.syntax.ast.DeclarationNode;
@@ -84,16 +97,66 @@ public class ViewByTypePlugin implements SmeltPlugin {
 
     private void presentObjectsByTypeView(DataModelPlugin dataModelPlugin, EditorModel editorModel,
             GUIExecutionContext context) {
-        context.createOrDisplay(ObjectsByTypeViewIdentifier.INSTANCE, BasicGUIPosition.LEFT, () -> {
-            DefaultMultipleCDockable dockable = new DefaultMultipleCDockable(null);
-            dockable.setTitleText("View by Type");
-            dockable.setCloseable(true);
-            dockable.setExternalizable(false); // TODO: why is this feature broken?
+        Supplier<MultipleCDockable> supplier = new Supplier<MultipleCDockable>() {
+            public MultipleCDockable get() {
+                // Create contents. We will display an innocuous label if the database is not yet loaded.
+                CardLayout cardLayout = new CardLayout(0, 0);
+                JPanel contents = new JPanel(cardLayout);
+
+                final String noDatabaseCard = "no-database";
+                JLabel noDatabaseLabel = new JLabel("No database loaded.");
+                noDatabaseLabel.setVerticalAlignment(SwingConstants.TOP);
+                contents.add(noDatabaseLabel, noDatabaseCard);
+
+                final String databaseCard = "database";
                 DatabaseTreeViewByTypePanel treeViewByTypePanel = new DatabaseTreeViewByTypePanel(dataModelPlugin,
                         editorModel);
-                dockable.add(treeViewByTypePanel);
+                contents.add(treeViewByTypePanel, databaseCard);
+
+                if (dataModelPlugin.getDatabase() == null) {
+                    cardLayout.show(contents, noDatabaseCard);
+                } else {
+                    cardLayout.show(contents, databaseCard);
+                }
+
+                // Whenever the database is loaded or unloaded, make sure to update the display accordingly.
+                EventListener<DataModelPluginEvent> cardUpdateListener = new EventListener<DataModelPluginEvent>() {
+                    @Override
+                    public void eventOccurred(DataModelPluginEvent event) {
+                        if (event instanceof DatabaseOpenedEvent) {
+                            cardLayout.show(contents, databaseCard);
+                        } else if (event instanceof DatabaseClosedEvent) {
+                            cardLayout.show(contents, noDatabaseCard);
+                        }
+                    }
+                };
+                dataModelPlugin.addListener(cardUpdateListener);
+
+                // Create basic dockable container.
+                DefaultMultipleCDockable dockable = new DefaultMultipleCDockable(null);
+                dockable.setTitleText("View by Type");
+                dockable.setCloseable(true);
+                dockable.setExternalizable(false); // TODO: why is this feature broken?
+                dockable.add(contents);
+
+                // If the dockable is destroyed, drop the card update listener.
+                dockable.addCDockableStateListener(new CDockableStateListener() {
+                    @Override
+                    public void visibilityChanged(CDockable dockable) {
+                        if (!dockable.isVisible()) {
+                            dataModelPlugin.removeListener(cardUpdateListener);
+                        }
+                    }
+
+                    @Override
+                    public void extendedModeChanged(CDockable dockable, ExtendedMode mode) {
+                    }
+                });
+
                 return dockable;
-            });
+            }
+        };
+        context.createOrDisplay(ObjectsByTypeViewIdentifier.INSTANCE, BasicGUIPosition.LEFT, supplier);
     }
 
     private static enum ObjectsByTypeViewIdentifier implements DockableIdentifier {
